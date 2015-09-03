8b1b69b1148ed9bf68dd136bc3b6ac5ceb7c1378
Berlin Sohn <berlinsohn@gmail.com>
2015-09-03 00:52:19 -0700

    finish styling and adding location features

--- weatherApp.js
+++ weatherApp.js
@@ -1,93 +1,90 @@
 $(document).ready(init);
 
 function init(){
-  //$('#goButton').click(goClicked);
-  $('#city').on('keypress', inputKeypress);
-$("#goButton").click(function(){
-  if($("#goButton").css("right") <= "-40px") 
-      {
-     $("#goButton").animate({right:'0%'});        
-      }   
-   else   {
-  $("#goButton").animate({right:'-30%'});      
-              }   
+  $('#goButton').click(locationEntered);
+  $('#currentLocation').click(currentLocationClicked);
+  $("#goButton").click(function(){
+    if($('#goButton').css("right") <= "-40px"){
+      $("#goButton").animate({right:'0%'});        
+    }else{
+      $("#goButton").animate({right:'-30%'});      
+    }   
+  });
+  $("#currentLocation").click(function(){
+    if($('#goButton').css("right") <= "-40px"){
+      $("#goButton").animate({right:'0%'});        
+    }else{
+      $("#goButton").animate({right:'-30%'});      
+    }   
   }); 
 }
 
+var currentCity = '';
+var currentStateorCountry = '';
+var zipCode = '';
 
-function inputKeypress(e){
-  if(e.which === 13){
-    goClicked(e);
-  }
-}
-
-var citySelected = $('#city').val();
-console.log(citySelected);
-
-function goClicked(e){
-  var promise = $.ajax('http://api.wunderground.com/api/751291fe8abdb495/conditions/q/'+ encodeURI($('#desiredState').val()) +'/'+ encodeURI($('#desiredCity').val()) +'.json',
-  {
+function currentLocationClicked(e){
+  var currentLocation = $.ajax('http://api.wunderground.com/api/751291fe8abdb495/geolookup/q/autoip.json',{
     dataType: 'jsonp',
     method: 'GET'
-
-  })
-  .success(function(data){
-  console.log('data:', data);
-  var citySelected = $('#desiredCity').val();
-  console.log(citySelected);
-  var stateSelected = $('#desiredState').val();
-  console.log(stateSelected);
-  
-  $('#city').text(data.current_observation.display_location.city + ', ');
-  $('#state').text(data.current_observation.display_location.state);
-  $('#temp').text(data.current_observation.temperature_string);
-  $('#time').text(data.current_observation.observation_time);
-  $('#humidity').text(data.current_observation.relative_humidity + ' humidity');
-  $('#weather').text(data.current_observation.weather);
-  
-    weeklyForcast(e);
-
-      function weeklyForcast(e){
-       var promise2 = $.ajax('http://api.wunderground.com/api/751291fe8abdb495/forecast10day/q/CA/San_Francisco.json',
-        {
-          dataType: 'jsonp',
-          method: 'GET'
-
-        }).success(function(data){
-          console.log('forcast:', data);
-         $('#weatherOfDay0').text(data.forecast.txt_forecast.forecastday[0].fcttext);
-         $('#weatherOfDay1').text(data.forecast.txt_forecast.forecastday[2].fcttext);
-         $('#weatherOfDay2').text(data.forecast.txt_forecast.forecastday[4].fcttext);
-         $('#weatherOfDay3').text(data.forecast.txt_forecast.forecastday[6].fcttext);
-         $('#weatherOfDay4').text(data.forecast.txt_forecast.forecastday[8].fcttext);
-         $('#weatherOfDay5').text(data.forecast.txt_forecast.forecastday[10].fcttext);
-         $('#weatherOfDay6').text(data.forecast.txt_forecast.forecastday[12].fcttext);
-         $('#weatherOfDay7').text(data.forecast.txt_forecast.forecastday[14].fcttext);
-         $('#weatherOfDay8').text(data.forecast.txt_forecast.forecastday[16].fcttext);
-         $('#weatherOfDay9').text(data.forecast.txt_forecast.forecastday[18].fcttext);
-         $('#dayOfWeek0').text(data.forecast.txt_forecast.forecastday[0].title);
-         $('#dayOfWeek1').text(data.forecast.txt_forecast.forecastday[2].title);
-         $('#dayOfWeek2').text(data.forecast.txt_forecast.forecastday[4].title);
-         $('#dayOfWeek3').text(data.forecast.txt_forecast.forecastday[6].title);
-         $('#dayOfWeek4').text(data.forecast.txt_forecast.forecastday[8].title);
-         $('#dayOfWeek5').text(data.forecast.txt_forecast.forecastday[10].title);
-         $('#dayOfWeek6').text(data.forecast.txt_forecast.forecastday[12].title);
-         $('#dayOfWeek7').text(data.forecast.txt_forecast.forecastday[14].title);
-         $('#dayOfWeek8').text(data.forecast.txt_forecast.forecastday[16].title);
-         $('#dayOfWeek9').text(data.forecast.txt_forecast.forecastday[18].title);
-
-      
-
-        }).fail(function(error){
-
-        });
-
-       }
-
-
-  })
-  .fail(function(error){
-  console.log('error:', error);
+  }).success(function(data){
+    console.log('Geodata', data);
+    currentCity = data.location.city + ', ';
+    currentStateorCountry = data.location.state;
+    currentCountry = data.location.country;
+    goClicked();  
+  }).fail(function(error){
+     $('#weatherCheck').text('oops! we can find you! try typing your location instead');
   });
-  console.log('promise', promise);
+}
+function locationEntered(){
+  currentCity = $('#desiredCity').val();
+  currentStateorCountry = $('#desiredState').val();
+  zipcode = $('#desiredZip').val();
+  goClicked();
+}
+function goClicked(e){
+  var promise = $.ajax('http://api.wunderground.com/api/751291fe8abdb495/conditions/q/'+ encodeURI(currentStateorCountry) +'/'+ encodeURI(currentCity || zipcode) +'.json', {
+    dataType: 'jsonp',
+    method: 'GET'
+  }).success(function(data){
+    $('#city').text(data.current_observation.display_location.city + ', ');
+    $('#state').text(data.current_observation.display_location.state);
+    $('#temp').text(data.current_observation.temperature_string);
+    $('#time').text(data.current_observation.observation_time);
+    $('#humidity').text(data.current_observation.relative_humidity + ' humidity');
+    $('#weather').text(data.current_observation.weather);
+    weeklyForcast(e);
+    function weeklyForcast(e){
+      var promise2 = $.ajax('http://api.wunderground.com/api/751291fe8abdb495/forecast10day/q/CA/San_Francisco.json', {
+        dataType: 'jsonp',
+        method: 'GET'
+        }).success(function(data){
+           $('#weatherOfDay0').text(data.forecast.txt_forecast.forecastday[0].fcttext);
+           $('#weatherOfDay1').text(data.forecast.txt_forecast.forecastday[2].fcttext);
+           $('#weatherOfDay2').text(data.forecast.txt_forecast.forecastday[4].fcttext);
+           $('#weatherOfDay3').text(data.forecast.txt_forecast.forecastday[6].fcttext);
+           $('#weatherOfDay4').text(data.forecast.txt_forecast.forecastday[8].fcttext);
+           $('#weatherOfDay5').text(data.forecast.txt_forecast.forecastday[10].fcttext);
+           $('#weatherOfDay6').text(data.forecast.txt_forecast.forecastday[12].fcttext);
+           $('#weatherOfDay7').text(data.forecast.txt_forecast.forecastday[14].fcttext);
+           $('#weatherOfDay8').text(data.forecast.txt_forecast.forecastday[16].fcttext);
+           $('#weatherOfDay9').text(data.forecast.txt_forecast.forecastday[18].fcttext);
+           $('#dayOfWeek0').text(data.forecast.txt_forecast.forecastday[0].title);
+           $('#dayOfWeek1').text(data.forecast.txt_forecast.forecastday[2].title);
+           $('#dayOfWeek2').text(data.forecast.txt_forecast.forecastday[4].title);
+           $('#dayOfWeek3').text(data.forecast.txt_forecast.forecastday[6].title);
+           $('#dayOfWeek4').text(data.forecast.txt_forecast.forecastday[8].title);
+           $('#dayOfWeek5').text(data.forecast.txt_forecast.forecastday[10].title);
+           $('#dayOfWeek6').text(data.forecast.txt_forecast.forecastday[12].title);
+           $('#dayOfWeek7').text(data.forecast.txt_forecast.forecastday[14].title);
+           $('#dayOfWeek8').text(data.forecast.txt_forecast.forecastday[16].title);
+           $('#dayOfWeek9').text(data.forecast.txt_forecast.forecastday[18].title);
+        }).fail(function(error){
+           $('#weatherCheck').text("oops. we can't seem to find that data" );
+      });
+    }
+  }).fail(function(error){
+     $('#weatherCheck').text("oops. we can't seem to find that location");
+  });
 }
