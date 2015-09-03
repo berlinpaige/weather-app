9e8b6b61f4a187e3fa418532fa6d270a786aa659
Berlin Sohn <berlinsohn@gmail.com>
2015-09-02 23:11:35 -0700

    make beautiful

--- weatherApp.js
+++ weatherApp.js
@@ -1,9 +1,17 @@
 $(document).ready(init);
 
 function init(){
-  $('#goButton').click(goClicked);
+  //$('#goButton').click(goClicked);
   $('#city').on('keypress', inputKeypress);
-
+$("#goButton").click(function(){
+  if($("#goButton").css("right") <= "-40px") 
+      {
+     $("#goButton").animate({right:'0%'});        
+      }   
+   else   {
+  $("#goButton").animate({right:'-30%'});      
+              }   
+  }); 
 }
 
 
@@ -22,7 +30,6 @@ function goClicked(e){
     dataType: 'jsonp',
     method: 'GET'
 
-
   })
   .success(function(data){
   console.log('data:', data);
@@ -37,6 +44,47 @@ function goClicked(e){
   $('#time').text(data.current_observation.observation_time);
   $('#humidity').text(data.current_observation.relative_humidity + ' humidity');
   $('#weather').text(data.current_observation.weather);
+  
+    weeklyForcast(e);
+
+      function weeklyForcast(e){
+       var promise2 = $.ajax('http://api.wunderground.com/api/751291fe8abdb495/forecast10day/q/CA/San_Francisco.json',
+        {
+          dataType: 'jsonp',
+          method: 'GET'
+
+        }).success(function(data){
+          console.log('forcast:', data);
+         $('#weatherOfDay0').text(data.forecast.txt_forecast.forecastday[0].fcttext);
+         $('#weatherOfDay1').text(data.forecast.txt_forecast.forecastday[2].fcttext);
+         $('#weatherOfDay2').text(data.forecast.txt_forecast.forecastday[4].fcttext);
+         $('#weatherOfDay3').text(data.forecast.txt_forecast.forecastday[6].fcttext);
+         $('#weatherOfDay4').text(data.forecast.txt_forecast.forecastday[8].fcttext);
+         $('#weatherOfDay5').text(data.forecast.txt_forecast.forecastday[10].fcttext);
+         $('#weatherOfDay6').text(data.forecast.txt_forecast.forecastday[12].fcttext);
+         $('#weatherOfDay7').text(data.forecast.txt_forecast.forecastday[14].fcttext);
+         $('#weatherOfDay8').text(data.forecast.txt_forecast.forecastday[16].fcttext);
+         $('#weatherOfDay9').text(data.forecast.txt_forecast.forecastday[18].fcttext);
+         $('#dayOfWeek0').text(data.forecast.txt_forecast.forecastday[0].title);
+         $('#dayOfWeek1').text(data.forecast.txt_forecast.forecastday[2].title);
+         $('#dayOfWeek2').text(data.forecast.txt_forecast.forecastday[4].title);
+         $('#dayOfWeek3').text(data.forecast.txt_forecast.forecastday[6].title);
+         $('#dayOfWeek4').text(data.forecast.txt_forecast.forecastday[8].title);
+         $('#dayOfWeek5').text(data.forecast.txt_forecast.forecastday[10].title);
+         $('#dayOfWeek6').text(data.forecast.txt_forecast.forecastday[12].title);
+         $('#dayOfWeek7').text(data.forecast.txt_forecast.forecastday[14].title);
+         $('#dayOfWeek8').text(data.forecast.txt_forecast.forecastday[16].title);
+         $('#dayOfWeek9').text(data.forecast.txt_forecast.forecastday[18].title);
+
+      
+
+        }).fail(function(error){
+
+        });
+
+       }
+
+
   })
   .fail(function(error){
   console.log('error:', error);
